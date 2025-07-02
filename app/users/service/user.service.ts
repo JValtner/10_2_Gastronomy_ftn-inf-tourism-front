c4043cdd18891c60998b1e4a7c3232252057906a
Josip Valtner <josipvaltner@gmail.com>
2025-07-02 21:33:28 +0200

    radna verzija

--- app/users/service/user.service.ts
+++ app/users/service/user.service.ts
@@ -32,23 +32,7 @@ export class UserService {
         console.error("Login error:", error.message);
         throw error;
       });
-  }
-
-  redirect(user?: User): void {
-    if (user) {
-      if (user.role === "vlasnik") {
-        window.location.href = `/app/restaurants/pages/restaurants/restaurants.html?ownerId=${user.id}`;
-      } else if (user.role === "vodic") {
-        window.location.href = "/app/tours/pages/userTours/userTours.html";
-      } else if (user.role === "turista") {
-        window.location.href = "#";
-      } else {
-        window.location.href = "/app/users/pages/login/login.html";
-      }
-    }
-  }
-
-  
+  }  
 }
 
 
